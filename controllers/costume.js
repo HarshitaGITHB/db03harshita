1ebe492536e4daad3fb9f805b7315812d3ee5820
HarshitaGITHB <harshithalaxmanrao@gmail.com>
2021-11-12 09:58:37 -0600

    new commit

--- controllers/costume.js
+++ controllers/costume.js
@@ -49,9 +49,7 @@ exports.costume_create_post = async function(req, res) {
 // Handle Costume delete form on DELETE. 
 exports.costume_delete = function(req, res) { 
     res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id); 
-}; 
- 
-// Handle Costume update form on PUT. 
+};  
 //Handle Costume update form on PUT. 
 exports.costume_update_put = async function(req, res) { 
     console.log(`update on id ${req.params.id} with body 
